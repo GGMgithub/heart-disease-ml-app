031ac8ddacb29c61fd2e41cc922d88675148c8d3
GGMgithub <gouthamgm7@gmail.com>
2026-01-21 17:30:47 +0530

    Update train_and_save_models.py

--- train_and_save_models.py
+++ train_and_save_models.py
@@ -66,6 +66,7 @@ y = df["target"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
+
 # ===============================
 # Feature Scaling
 # ===============================
